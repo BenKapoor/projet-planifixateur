2c4ccacd05ded50e18640725f264150667c05c87
Benjamin <benjaminkapoor@yahoo.fr>
2021-05-01 19:39:50 +0200

    commit File Controller/Repo/Service/Exceptions/response et Message

---
+++ projet-planifixateur/src/main/java/com/planifixateur/service/FileStorageService.java
@@ -0,0 +1,40 @@
+package com.planifixateur.service;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.planifixateur.dao.FileDBRepository;
+import com.planifixateur.model.FileDB;
+
+@Service
+public class FileStorageService {
+
+	@Autowired
+	private FileDBRepository fileDBRepository;
+
+	public FileDB store(MultipartFile file) throws IOException {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+
+		return fileDBRepository.save(FileDB);
+	}
+
+	public FileDB getFile(String id) {
+		return fileDBRepository.findById(id).get();
+	}
+
+	public Stream<FileDB> getAllFiles() {
+		return fileDBRepository.findAll().stream();
+	}
+
+	public FileDB deleteFile(String id) {
+		FileDB filedb = getFile(id);
+		fileDBRepository.deleteById(id);
+		return filedb;
+	}
+}
